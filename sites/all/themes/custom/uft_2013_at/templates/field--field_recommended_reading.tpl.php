e7b63ed63c415abe9fad45e44633fb1e97376399
Claudine Chionh <cc@claudinec.net>
2013-07-18 16:32:40 +1000

    update field templates for learning outcomes and recommended reading

--- sites/all/themes/custom/uft_2013_at/templates/field--field_recommended_reading.tpl.php
+++ sites/all/themes/custom/uft_2013_at/templates/field--field_recommended_reading.tpl.php
@@ -85,7 +85,7 @@
   <?php endif; ?>
 
   <div class="field-items"<?php print $content_attributes; ?>>
-  <p>Upon successful completion of this unit, it is expected that students will be able to:</p>
+  <p>* = set texts recommended for purchase</p>
   <ul>
     <?php foreach ($items as $delta => $item) : ?>
       <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>>
